5274ab2be7d9de6f006592842d9a3cedaeba5301
Maclenn77 <maclenn77@gmail.com>
2022-04-10 16:36:49 -0500

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :v1 do
+    resources :sessions, only: [:create, :destroy]
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
