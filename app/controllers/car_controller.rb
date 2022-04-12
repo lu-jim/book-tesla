d3e7218dc9ed23f948ab26f9ee6da5949eebd2ec
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-12 17:51:24 -0400

    Generate user and car controllers

---
+++ app/controllers/car_controller.rb
@@ -0,0 +1,23 @@
+class V1::UsersController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    render json: User.all.order(created_at: :asc)
+  end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create; end
+
+  private
+
+  def user_params
+    params.permit(
+      :username,
+      :email,
+      :password
+    )
+  end
+end
