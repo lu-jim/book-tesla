153b57e523e668e27e349a621781295f488a46e8
Maclenn77 <maclenn77@gmail.com>
2022-04-13 15:30:05 -0500

    Solve linters

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,10 @@
 class MembersController < ApplicationController
-    #Controller to test logged in vs logged out behaviour on an
-    #endpoint that required authenticating
-    
-    before_action :authenticate_user!
-  
-    def show
-      render json: { message: "Yeppa you did it" }
-    end
+  # Controller to test logged in vs logged out behaviour on an
+  # endpoint that required authenticating
+
+  before_action :authenticate_user!
+
+  def show
+    render json: { message: 'Yeppa you did it' }
   end
+end
