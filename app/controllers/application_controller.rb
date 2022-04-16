97a80c9e06e96b5024558c29aa0fd1772c9b18db
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-15 23:05:50 -0400

    Concile merge with authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::API
-<<<<<<< HEAD
   include Response
   include ExceptionHandler
-=======
-  include ActionController::MimeResponds
-  :respond_to => json
->>>>>>> dev
 end
