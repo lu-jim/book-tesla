27c1e71af4dc4415106df38519629e190f18f2d1
Maclenn77 <maclenn77@gmail.com>
2022-04-21 08:01:12 -0500

    Update API Documentation

--- spec/requests/v1/bookings_spec.rb
+++ spec/requests/v1/bookings_spec.rb
@@ -37,6 +37,21 @@ RSpec.describe 'v1/bookings', type: :request do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
+    get('show booking') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
     patch('update booking') do
       response(200, 'successful') do
         let(:id) { '123' }
